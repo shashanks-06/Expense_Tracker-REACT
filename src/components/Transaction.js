d60027d9286824cfed833a62f8ab0989930acf16
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-05-09 23:24:39 +0530

    Transactions color border and sign

---
+++ src/components/Transaction.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Transaction = ({ transaction }) => {
+  const sign = transaction.amount > 0 ? "+" : "-";
+  return (
+    <li className={transaction.amount > 0 ? "plus" : "minus"}>
+      {transaction.text}
+      <span>
+        {sign}$ {Math.abs(transaction.amount)}
+      </span>
+      <button className="delete-btn">x</button>
+    </li>
+  );
+};
+
+export default Transaction;
