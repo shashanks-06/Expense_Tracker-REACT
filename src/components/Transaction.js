52634458abcb141d17e21cb4118cd7b35f877d65
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-05-10 17:27:42 +0530

    Deletion of transactions is handle

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
   const sign = transaction.amount > 0 ? "+" : "-";
+
   return (
     <li className={transaction.amount > 0 ? "plus" : "minus"}>
       {transaction.text}
       <span>
         {sign}$ {Math.abs(transaction.amount)}
       </span>
-      <button className="delete-btn">x</button>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
     </li>
   );
 };
