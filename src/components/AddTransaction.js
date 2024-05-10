0760aa8e2641d83fd8317ea971f0a3aeabea2bb7
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-05-10 17:48:10 +0530

    Addition of transactions is handle

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 10000000),
+      text,
+      amount: +amount, // to add amount as a number otherwise it get added as a string
+    };
+
+    addTransaction(newTransaction);
+  };
+
   return (
     <>
       <h3>Add New Transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
